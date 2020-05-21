308c437d9889a883675e97a7bb594a3d8ace8c75
Jose Manuel Zelezarof <zelezarof@gmail.com>
2020-05-21 13:04:12 -0500

    Remove dracula theme from quteb's config folder Minor updates to preferences

--- qutebrowser/.config/qutebrowser/config.py
+++ qutebrowser/.config/qutebrowser/config.py
@@ -1,15 +1,6 @@
-import dracula.draw
-
 # Load existing settings made via :set
 config.load_autoconfig()
 
-dracula.draw.blood(c, {
-    'spacing': {
-        'vertical': 6,
-        'horizontal': 8
-    }
-})
-
+# Set custom properties
 config.set('qt.highdpi', True)
-config.set('url.start_pages', ['https://www.gitlab.com/zelezarof'])
-config.set('tabs.show', 'never')
+config.set('url.start_pages', ['https://www.github.com/zelezarof'])
