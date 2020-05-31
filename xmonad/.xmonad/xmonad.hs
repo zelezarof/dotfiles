59cb28fbb080506e156265d8fc55b24b169abf20
Jose Manuel Zelezarof <zelezarof@gmail.com>
2020-05-31 14:10:14 -0500

    Adding smartBorder support

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -2,15 +2,18 @@ import XMonad
 import XMonad.Config.Desktop
 import XMonad.Util.SpawnOnce
 
+import XMonad.Layout.NoBorders (smartBorders)
 import XMonad.Layout.Spacing
 
-baseConfig = desktopConfig
 
-main = xmonad baseConfig {
+mySpacing = spacingRaw True (Border 0 5 5 5) True (Border 5 5 5 5) True
+
+
+main = xmonad desktopConfig {
   borderWidth         = 4,
   terminal            = "alacritty",
   normalBorderColor   = "#928374",
   focusedBorderColor  = "#689d6a",
-  layoutHook          = spacingRaw True (Border 0 5 5 5) True (Border 5 5 5 5) True $ layoutHook def,
-  startupHook         = startupHook baseConfig <+> spawnOnce "alacritty"
+  layoutHook          = smartBorders $ mySpacing $ layoutHook def, 
+  startupHook         = startupHook desktopConfig <+> spawnOnce "alacritty"
 }
