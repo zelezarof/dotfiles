5c9ee3788200249298fe23ea70207acad9e243b7
Jose M Zelezarof <zelezarof@gmail.com>
2024-05-26 14:32:16 -0400

    Update keybinds

--- nvim/dot-config/nvim/lua/zelezarof/core/settings.lua
+++ nvim/dot-config/nvim/lua/zelezarof/core/settings.lua
@@ -1,9 +1,39 @@
 vim.cmd("let g:netrw_liststyle = 1")
 
+vim.opt.guicursor = ""
+
 -- line numbers
 vim.opt.relativenumber = true
 vim.opt.number = true
 
+-- tabs / spaces
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.wrap = false
+
+-- swap / backup / undo
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("XDG_DATA_HOME") .. "vim/undodir"
+vim.opt.undofile = true
+
+-- search
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+vim.opt.colorcolumn = "120"
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+
+vim.opt.updatetime = 50
+
+-- enable folding
 vim.opt.foldenable = false
 vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
--- nvim/dot-config/nvim/lua/zelezarof/plugins/theme.lua
+++ nvim/dot-config/nvim/lua/zelezarof/plugins/theme.lua
@@ -1,9 +1,9 @@
-return { 
-	"rose-pine/neovim", 
+return {
+	"rose-pine/neovim",
 	name = "rose-pine",
 	config = function()
 		vim.cmd.colorscheme("rose-pine-moon")
-		vim.api.nvim_set_hl(0, "Normal", {bg = "none" })
-		vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none" })
+		vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 	end
 }
