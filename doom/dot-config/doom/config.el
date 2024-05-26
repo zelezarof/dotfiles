45e7cee2be0f58bcc92a2aa723f109eb58f279f9
Jose M Zelezarof <zelezarof@gmail.com>
2024-05-26 13:57:42 -0400

    Add emacs themes

--- doom/dot-config/doom/config.el
+++ doom/dot-config/doom/config.el
@@ -21,7 +21,7 @@
 ;; See 'C-h v doom-font' for documentation and more examples of what they
 ;; accept. For example:
 ;;
-(setq doom-font (font-spec :family "M+CodeLat60 Nerd Font Mono" :size 17 :weight 'semibold)
+(setq doom-font (font-spec :family "M+CodeLat60 Nerd Font Mono" :size 17 :weight 'regular)
      doom-variable-pitch-font (font-spec :family "M+CodeLat60 Nerd Font Mono" :size 17))
 ;;
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
@@ -32,7 +32,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-gruvbox)
+(setq doom-theme 'doom-rose-pine-moon)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
@@ -40,7 +40,7 @@
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org/")
+(setq org-directory "~/Documents/org/")
 
 
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
